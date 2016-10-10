8fe2ef5ad3b379c2554f80b522aab6b43bbd7909
whitekkk <white_itsme@hotmail.com>
2016-10-10 20:09:03 +0700

    movement

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div @mousemove="mousePosition()">
-    <div id="app"  @mouseover="move" @mouseout="outOfArea">
+    <div id="screen"  @mouseover="move" @mouseout="outOfArea">
       <div class="avatar" :style="{'top': avatar.y-25 + 'px', 'left': avatar.x-25 + 'px'}" v-for="avatar in avatars">
         <a style="margin: 10px;">
           {{avatar.name}}
         </a>
         <div>
-          <img src="../img/king.png" width="65" height="65" style="position: absolute; z-index: 999; top:5px; left:-1px;"/>
-          <img src="../img/1.png" width="60" height="60" style="position: absolute; z-index: 99;"/>
-          <img src="../img/LEG1.png" width="10" height="10" style="position: absolute; z-index: 99; top:75px; left:20px;"/>
-          <img src="../img/LEG1.png" width="10" height="10" style="position: absolute; z-index: 99; top:75px; left:30px;"/>
+          <!-- <img src="../static/img/king.png" width="65" height="65" style="position: absolute; z-index: 999; top:5px; left:-1px;"/>
+          -->
+          <img :src="'../static/img/' + avatar.body + '.png'" width="60" height="60" style="position: absolute; z-index: 99;"/>
+          <img :src="'../static/img/LEG' + avatar.leg1 + '.png'" width="10" height="10" style="position: absolute; z-index: 99; top:75px; left:20px;"/>
+          <img :src="'../static/img/LEG' + avatar.leg2 + '.png'" height="10" style="position: absolute; z-index: 99; top:75px; left:30px;"/>
         </div>
       </div>
     </div>
@@ -29,6 +30,10 @@ var config = {
 firebase.initializeApp(config)
 
 var Avatars = firebase.database().ref('avatars')
+var myId = ''
+window.onbeforeunload = function () {
+  firebase.database().ref('avatars/' + myId).remove()
+}
 
 export default {
   ready () {
@@ -44,9 +49,16 @@ export default {
       var avatar = vm.avatars.find(avatar => avatar.id === id)
       avatar.x = snapshot.val().x
       avatar.y = snapshot.val().y
+      avatar.body = snapshot.val().body
+      avatar.leg1 = snapshot.val().leg1
+      avatar.leg2 = snapshot.val().leg2
+      // change
       if (vm.myAvatar.id === id) {
         vm.myAvatar.x = snapshot.val().x
         vm.myAvatar.y = snapshot.val().y
+        vm.myAvatar.body = snapshot.val().body
+        vm.myAvatar.leg1 = snapshot.val().leg1
+        vm.myAvatar.leg2 = snapshot.val().leg2
       }
     })
     Avatars.on('child_removed', function (snapshot) {
@@ -55,13 +67,13 @@ export default {
     })
   },
   data () {
-    let chickColor = Math.floor(Math.random() * 3)
     // let r = Math.floor(Math.random() * 255)
     // let g = Math.floor(Math.random() * 255)
     // let b = Math.floor(Math.random() * 255)
 
     let x = Math.floor(Math.random() * 500)
     let y = Math.floor(Math.random() * 500)
+    let body = Math.floor(Math.random() * 3) + 1
 
     return {
       mouseX: 0,
@@ -69,10 +81,15 @@ export default {
       avatars: [],
       myAvatar: {
         name: 'Ponzi',
-        color: `${chickColor}`, // color: `rgb(${r}, ${g}, ${b})`,
+        // color: `${chickColor}`, // color: `rgb(${r}, ${g}, ${b})`,
         x,
         y,
-        active: false
+        active: false,
+        body: body + '-7',
+        color: body,
+        leg1: '1',
+        leg2: '1',
+        king: false
       }
     }
   },
@@ -86,6 +103,7 @@ export default {
     addAvatar: function (newAvatar) {
       let result = Avatars.push(newAvatar)
       this.myAvatar.id = result.key
+      myId = this.myAvatar.id
     },
     move () {
       let vm = this
@@ -99,7 +117,7 @@ export default {
         var checkX = (xOrigin < x1) ? 1 : -1
         var checkY = (yOrigin < y1) ? 1 : -1
         var err = dx - dy
-        if (vm.myAvatar.y < 500 && vm.myAvatar.active) {
+        if (vm.myAvatar.active) {
           firebase.database().ref('avatars/' + vm.myAvatar.id).update({
             y: yOrigin,
             x: xOrigin
@@ -115,6 +133,7 @@ export default {
           err += dx
           yOrigin += checkY
         }
+        vm.actionLeg()
       }, 10)
     },
     outOfArea () {
@@ -130,21 +149,102 @@ export default {
       let position = window.event
       vm.mouseX = position.clientX
       vm.mouseY = position.clientY
+      vm.actionChick()
+    },
+    actionChick () {
+      let vm = this
+      let body = vm.myAvatar.color.toString()
+      let slope = (vm.myAvatar.y - vm.mouseY) / (vm.myAvatar.x - vm.mouseX)
+      console.log(slope)
+      if (vm.mouseX > vm.myAvatar.x && vm.mouseY > vm.myAvatar.y) { // q 4
+        if (slope < 0.25) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-1'
+          })
+        } else if (slope < 0.75) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-8'
+          })
+        } else {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-7'
+          })
+        }
+      } else if (vm.mouseX > vm.myAvatar.x && vm.mouseY < vm.myAvatar.y) { // q1
+        if (slope < -0.75) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-3'
+          })
+        } else if (slope < -0.25) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-2'
+          })
+        } else {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-1'
+          })
+        }
+      } else if (vm.mouseX < vm.myAvatar.x && vm.mouseY < vm.myAvatar.y) { // q2
+        if (slope < 0.25) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-5'
+          })
+        } else if (slope < 0.75) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-4'
+          })
+        } else {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-3'
+          })
+        }
+      } else if (vm.mouseX < vm.myAvatar.x && vm.mouseY > vm.myAvatar.y) { // q3
+        if (slope > -0.25) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-5'
+          })
+        } else if (slope > -0.75) {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-6'
+          })
+        } else {
+          firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+            body: body + '-7'
+          })
+        }
+      }
+    },
+    actionLeg () {
+      let vm = this
+      if (vm.myAvatar.x % 20 === 0 || vm.myAvatar.y % 20 === 0) {
+        firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+          leg1: '1',
+          leg2: '3'
+        })
+      } else if (vm.myAvatar.x % 10 === 0 || vm.myAvatar.y % 10 === 0) {
+        firebase.database().ref('avatars/' + vm.myAvatar.id).update({
+          leg1: '2',
+          leg2: '1'
+        })
+      }
     }
   }
 }
 </script>
 
 <style>
-#app {
-  width: 500px;
-  height: 500px;
-  background: #ccc;
+html {
+  background-image: url("../static/img/gird-bg.png");
+}
+#screen {
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
 }
 #outOfArea {
-  width: 1000px;
-  height: 1000px;
-  background: #0cc;
+  width: 100vw;
+  height: 100vh;
 }
 .avatar {
   position: absolute;
